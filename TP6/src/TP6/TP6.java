04230605e842ec62d127016beeb591971b467f28
Rodrigo Ferron <ro797749@gmail.com>
2025-09-14 13:14:03 -0300

    TP6

---
+++ TP6/src/TP6/TP6.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP6;
+
+import Vista.DeTodoSA;
+
+public class TP6 {
+
+    public static void main(String[] args) {
+        DeTodoSA deTodo = new DeTodoSA();
+        deTodo.setVisible(true);
+    }
+    
+}
